73757e8972ba47caee6b6abeeb5ee64d677bd78e
Ezekiel-66 <133562136+Ezekiel-66@users.noreply.github.com>
2024-07-16 22:57:36 +0100

    Update set.cjs

--- set.cjs
+++ set.cjs
@@ -20,13 +20,13 @@ const config = {
   AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
   
   
-  AUTO_REJECT_CALLS: process.env.AUTO_REJECT_CALLS !== undefined ? process.env.REJECT_CALL === 'true' : false, 
+  AUTO_REJECT_CALLS: process.env.AUTO_REJECT_CALLS !== undefined ? process.env.REJECT_CALL === 'false' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || 'private',
   PREFIX: process.env.PREFIX || '.',
   PORT: process.env.PORT || "5000", /* Where App Will Listen On */
-  OWNER_NAME: process.env.OWNER_NAME || "ɢɪғᴛᴇᴅ ᴛᴇᴄʜ",
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "254762016957",
+  OWNER_NAME: process.env.OWNER_NAME || "*☛✎༒☆𝔈𝔷𝔷𝔶𝔪𝔬𝔫𝔢𝔶☆༒✎☚*",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "2347064746124",
   WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
 
